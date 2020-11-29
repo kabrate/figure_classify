f28bce7a398a90feb39771977b322f586113c397
kabrate <937160666@qq.com>
2020-11-30 01:58:36 +0800

    add load

--- figure_classify/figure_classify.py
+++ figure_classify/figure_classify.py
@@ -13,26 +13,31 @@ import copy
 import matplotlib.pyplot as plt
 
 
-input_size=224
-batch_size = 128
+input_size = 500
+batch_size = 64
 
 # 数据的读取
 data_dir = "./trainsleep"
 train_imgs = datasets.ImageFolder(os.path.join(data_dir, "train"), transforms.Compose([
+            transforms.Resize(input_size),
             transforms.RandomResizedCrop(input_size),
-            transforms.RandomHorizontalFlip(),
+            #transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            #Cutout(n_holes=1, length=32)
         ]))
-train_data = torch.utils.data.DataLoader(train_imgs, batch_size=batch_size)
+
+print(train_imgs.classes)
+train_data = torch.utils.data.DataLoader(train_imgs, batch_size=batch_size, shuffle=True)
 
 test_imgs = datasets.ImageFolder(os.path.join(data_dir, "test"), transforms.Compose([
+            transforms.Resize(input_size),
             transforms.RandomResizedCrop(input_size),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ]))
-test_data = torch.utils.data.DataLoader(test_imgs, batch_size=batch_size)
-print(train_data)
+test_data = torch.utils.data.DataLoader(test_imgs, batch_size=batch_size, shuffle=False)
+#print(train_data)
 
 
 # 模型的建立
@@ -46,15 +51,16 @@ optimizer = optim.Adam(model.parameters(),lr=0.0001)
 print('device:',device)
 print(model)
 
-epoch = 40
+epoch = 150
 
 # for _,[img,label] in enumerate(train_data):
 #     print(img)
 #     print(label)
-acc_train = np.zeros(401)
-acc_test = np.zeros(20)
+#acc_train = np.zeros(401)
+#acc_test = np.zeros(20)
 k = 0
 acc_best = 0
+q = 5
 for epo in range(1,epoch+1):
     logger = 'epoch:{}'.format(epo)
     start_time = time.time()
@@ -64,6 +70,8 @@ for epo in range(1,epoch+1):
     for _,[img,labels] in enumerate(train_data):
         inputs, labels = img.to(device).float(), labels.to(device)
         optimizer.zero_grad()
+        #if hasattr(torch.cuda, 'empty_cache'):
+            #torch.cuda.empty_cache()
         outputs = model(inputs)
         loss = criterion(outputs, labels)
         loss.backward()
@@ -74,10 +82,10 @@ for epo in range(1,epoch+1):
     logger += ', train loss:{:.6f}'.format(train_loss)
     logger += ', train acc:{:.6f}'.format(num_correct/len(train_data.dataset))
     logger = 'Time:{}, '.format(int(time.time()-start_time)) + logger
-    acc_train[epo] = num_correct/len(train_data.dataset)
+    #acc_train[epo] = num_correct/len(train_data.dataset)
     print(logger)
 
-    if epo % 20 == 0:
+    if epo % 5 == 0:
         with torch.no_grad():
             model.eval()
             test_loss = 0
@@ -93,17 +101,18 @@ for epo in range(1,epoch+1):
             logger1 += ', test acc:{:.6f}'.format(num_correct / len(test_data.dataset))
             logger1 = 'Time:{}, '.format(int(time.time() - start_time)) + logger1
             print(logger1)
-            acc_test[k] = num_correct / len(test_data.dataset)
-            if(acc_test[k]>acc_best):
-                torch.save(model.state_dict(), 'model.pth')
+            #acc_test[k] = num_correct / len(test_data.dataset)
+            #if(acc_test[k]>acc_best):
+            torch.save(model.state_dict(), 'model'+str(q)+'.pth')
 
             k = k+1
+            q = q+5
 
 
-plt.figure(1)
-plt.plot(acc_train)
-plt.show()
+#plt.figure(1)
+#plt.plot(acc_train)
+#plt.show()
 
-plt.figure(2)
-plt.plot(acc_test)
-plt.show()
+#plt.figure(2)
+#plt.plot(acc_test)
+#plt.show()
---
+++ figure_classify/figure_classify.spec
@@ -0,0 +1,33 @@
+# -*- mode: python ; coding: utf-8 -*-
+
+block_cipher = None
+
+
+a = Analysis(['figure_classify.py'],
+             pathex=['C:\\Users\\jing\\source\\repos\\figure_classify\\figure_classify'],
+             binaries=[],
+             datas=[],
+             hiddenimports=[],
+             hookspath=[],
+             runtime_hooks=[],
+             excludes=[],
+             win_no_prefer_redirects=False,
+             win_private_assemblies=False,
+             cipher=block_cipher,
+             noarchive=False)
+pyz = PYZ(a.pure, a.zipped_data,
+             cipher=block_cipher)
+exe = EXE(pyz,
+          a.scripts,
+          a.binaries,
+          a.zipfiles,
+          a.datas,
+          [],
+          name='figure_classify',
+          debug=False,
+          bootloader_ignore_signals=False,
+          strip=False,
+          upx=True,
+          upx_exclude=[],
+          runtime_tmpdir=None,
+          console=True )
